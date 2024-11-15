56bc0fc2e14676b6428a1879086bf8bf6628ff3d
David Hall <dlwh@stanford.edu>
2024-11-14 21:46:50 -0800

    test unions

--- tests/test_union.py
+++ tests/test_union.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from dataclasses import dataclass, field
 from typing import Dict, Union
 
@@ -95,69 +97,76 @@ def test_union_error_message_nested():
     )
 
 
+@dataclass
+class Baz_u:
+    z: int
+
+
+@dataclass
+class Foo_u(TestSetup):
+    x: Union[bool, Baz_u]
+
+
 def test_decode_union_with_dataclass_and_atomic():
-    @dataclass
-    class Baz:
-        z: int
-
-    @dataclass
-    class Foo(TestSetup):
-        x: Union[bool, Baz]
-
-    foo = Foo.setup("--x false")
+    foo = Foo_u.setup("--x false")
     assert foo.x is False
 
-    foo = Foo.setup("--x.z 1")
-    assert foo.x == Baz(z=1)
+    foo = Foo_u.setup("--x.z 1")
+    assert foo.x == Baz_u(z=1)
 
     try:
-        foo = Foo.setup("--x.z 1.2")
+        foo = Foo_u.setup("--x.z 1.2")
         raise AssertionError()
     except DecodingError:
         pass
 
 
+@dataclass
+class Baz_e:
+    z: int
+    y: str
+
+
+@dataclass
+class Bar_e:
+    z: bool
+
+
+@dataclass
+class Foo_e(TestSetup):
+    x: Union[Baz_e, Bar_e] = Bar_e(False)
+
+
 def test_union_error_message_dataclasses():
-    @dataclass
-    class Baz:
-        z: int
-        y: str
-
-    @dataclass
-    class Bar:
-        z: bool
-
-    @dataclass
-    class Foo(TestSetup):
-        x: Union[Baz, Bar] = 0
-
     with pytest.raises(DecodingError) as e:
-        Foo.setup("--x.z 1.2.3")
+        Foo_e.setup("--x.z 1.2.3")
 
     assert """`x`: Could not decode the value into any of the given types:
-    Baz: `z`: Couldn't parse '1.2.3' into an int
-    Bar: `z`: Couldn't parse '1.2.3' into a bool""".strip() in str(
+    Baz_e: `z`: Couldn't parse '1.2.3' into an int
+    Bar_e: `z`: Couldn't parse '1.2.3' into a bool""".strip() in str(
         e.value
     )
 
     with pytest.raises(DecodingError) as e:
-        Foo.setup("--x.y foo")
+        Foo_e.setup("--x.y foo")
 
     assert """`x`: Could not decode the value into any of the given types:
-    Baz: Missing required field(s) `z` for Baz
-    Bar: The fields `y` are not valid for Bar""".strip() in str(
+    Baz_e: Missing required field(s) `z` for Baz_e
+    Bar_e: The fields `y` are not valid for Bar_e""".strip() in str(
         e.value
     )
+
+
+@dataclass
+class Bar:
+    y: int
+
+
+@dataclass
+class Foo(TestSetup):
+    x: Optional[Union[Bar, Dict[str, Bar]]] = field(default=None)
 
 
 def test_union_argparse_dict():
-    @dataclass
-    class Bar:
-        y: int
-
-    @dataclass
-    class Foo(TestSetup):
-        x: Optional[Union[Bar, Dict[str, Bar]]] = field(default=None)
-
     foo = Foo.setup('--x \'{"a": {"y": 1}, "b": {"y": 2}}\'')
     assert foo.x == {"a": Bar(y=1), "b": Bar(y=2)}
