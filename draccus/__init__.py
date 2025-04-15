af6a477c7393407765555e461864797ee5455bbd
Jesse Rusak <me@jesserusak.com>
2025-04-15 10:51:40 -0400

    Onboarding nits

--- draccus/__init__.py
+++ draccus/__init__.py
@@ -1,6 +1,5 @@
 __version__ = "0.8.0"
 
-from . import utils, wrappers
 from .argparsing import parse, wrap
 from .cfgparsing import dump, load
 from .choice_types import CHOICE_TYPE_KEY, ChoiceRegistry, ChoiceType, PluginRegistry
@@ -12,3 +11,23 @@ from .utils import ParsingError
 
 get_config_type = Options.get_config_type
 set_config_type = Options.set_config_type
+
+__all__ = [
+    "CHOICE_TYPE_KEY",
+    "ChoiceRegistry",
+    "ChoiceType",
+    "ConfigType",
+    "Options",
+    "ParsingError",
+    "PluginRegistry",
+    "config_type",
+    "decode",
+    "dump",
+    "encode",
+    "field",
+    "get_config_type",
+    "load",
+    "parse",
+    "set_config_type",
+    "wrap",
+]
